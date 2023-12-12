b6b578ebdd9e26b15343451b3cbe01e6436615b9
iVinci-WaheedAkbarAli <akbar.ali@i-Vinci.co.jp>
2023-12-12 15:43:11 +0900

    initial commit

---
+++ contents/005/answer_005_1/ANSWER_005_1.java
@@ -0,0 +1,6 @@
+package answer_005_1;
+
+public class ANSWER_005_1 {
+  public static void main(String[] args) {
+  }
+}
---
+++ contents/005/answer_005_2/ANSWER_005_2.java
@@ -0,0 +1,6 @@
+package answer_005_2;
+
+public class ANSWER_005_2 {
+  public static void main(String[] args) {
+  }
+}
